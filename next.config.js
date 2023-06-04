5e0b0ac4138cf44d24865f20e03fe313fbff9cfc
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-06-04 18:53:34 +0200

    better image

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "flagcdn.com",
+        hostname: "**.flagcdn.com",
         port: "",
       },
       {
