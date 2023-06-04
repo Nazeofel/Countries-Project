90dbcc92447698da36889c04cd9bc0b854955dda
Alexander <96749659+Nazeofel@users.noreply.github.com>
2023-06-04 18:01:51 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { useHydrateAtoms } from "jotai/utils";
 import React, { useState } from "react";
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/countries");
+  const res = await fetch("/api/countries");
   const json = await res.json();
   const parsedCountries = json["data"];
 
